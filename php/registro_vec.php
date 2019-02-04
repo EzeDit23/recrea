9d3819e4df85071b684cd25d363a51accac5a95b
EzeDit23 <36055961+EzeDit23@users.noreply.github.com>
2019-02-04 17:05:32 -0300

    Primeros pasos back end

---
+++ php/registro_vec.php
@@ -0,0 +1,29 @@
+<?php
+    $email = $_POST["email"];
+    $clave = $_POST["clave"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $direccion = $_POST["direccion"];
+    $numero = $_POST["numero"];
+
+    include("conexion.php");
+
+    
+	$insertar = "INSERT INTO vecinos
+	(id,email,clave,nombre,apellido,direccion,numero) 
+	VALUES
+	(NULL,'$email','$clave','$nombre','$apellido','$direccion','$numero')";
+	
+	// 3. Ejecutar la query
+	$insertar_ej = mysqli_query(
+				$conexion, $insertar
+				);
+	
+	// 4. Verificar si funcionó
+	if($insertar_ej == true){
+        header('Location: gracias.html');
+	} else {
+		echo "ALgo pasa con el SQL";
+	}
+
+?>
---
+++ sql/sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE vecinos (
+id INT(6) AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(30) NOT NULL,
+clave VARCHAR(30) NOT NULL,
+nombre VARCHAR(50),
+apellido VARCHAR(50),
+direccion VARCHAR(50),
+numero INT(50)
+

9d168b674a8fbe0b39bdaec967f90060aa59054e
EzeDit23 <lovemytodied@hotmail.com>
2019-03-27 17:56:17 -0300

    main css

--- main.php
+++ main.php
@@ -26,28 +26,31 @@ include("components/menu.php");
                         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
                         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
                     </ol>
-                    <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/1.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption  d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <div class="carousel-inner">   
+                        <div class="carousel-item active">
+                            <img src="img/main/main-degrade.png" class="main-degrade" alt="">
+                            <img src="img/main/1.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-md-block">
+                                <div class="main-categ"><p>Comida</p></div>
+                                <p class="main-tit">VENI A DISFRUTAR!</p>
+                                <a href="https://goo.gl/maps/RDpMB1BCVu12" target="_blank"><p class="main-geo"><i class="fas fa-map-marker-alt"></i> Villa Bosch</p></a>
+                                <p class="main-copy">Veni a pasar la noche, cerveza artesanal, comida, buena musica y promos</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
                         </div>
-                    </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -60,8 +63,21 @@ include("components/menu.php");
                 </div>
             </div>
 
-            
+            <div class="col-md-12 sinNada cont-gral">
+                <h1 class="main-bien">BIENVENIDO A CUPONZ</h1>
+                <p class="main-bien-copy">Empieza ahora y ganate un cupon de los locales aderidos</p>
+            </div>
+
+            <div class="col-md-12 sinNada cont-btn-gral">
+                <div class="btn-gral">
+                    <a href="">GENERAR CUPON</a>
+                </div>
+                <a class="link-gral" href="" id="btn-miscupones">MIS CUPONES</a>
+            </div>
+
         </div>
+
+
     </div>
 
 
